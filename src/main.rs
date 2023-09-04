66e9c3e936754506efe74342413d4c174ab486b6
garandal245 <garandal245@gmail.com>
2023-09-04 02:24:52 -0400

    moved  user input for time to its own function fixed formatting.

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,55 @@
-        extern crate autopilot;
-        use std::{io, time::Duration};
-
-        fn main() {
-            let mut timer = 0;
-
-            println!("Enter max desired AFK time (in seconds)");
-
-            let usertime = loop {
-                let mut input = String::new();
-                io::stdin()
-                    .read_line(&mut input)
-                    .expect("Failed to read line");
-
-                match input.trim().parse::<i32>() {
-                    Ok(number) => {
-                        break number; // Exit the loop when a valid number is provided
-                    }
-                    Err(_) => {
-                        println!("Invalid input. Please enter a valid number.");
-                        continue; // Continue the loop if parsing fails
-                    }
-                }
-            };
+extern crate autopilot;
+use std::{io, time::Duration};
 
 
-            loop {
-                let cursor_locationold = autopilot::mouse::location();
-                std::thread::sleep(Duration::from_secs(1));
-                let cursor_locationnew = autopilot::mouse::location();
+fn get_user_time()-> i32 {
 
-                if cursor_locationold == cursor_locationnew {
-                    timer += 1;
-                    println!("{}", timer);
-                } else {
-                    timer = 0;
-                    println!("{}", timer);
-                }
+    loop {          // Loop until a valid number is provided.
 
-                if timer >= usertime {
-                    println!("You are AFK");
-                    break;
-                }
+        let mut input = String::new();  // Creates a mutable variable "input" as a string
+        io::stdin().read_line(&mut input)
+            .expect("Failed to read line"); // Calls io::stdin.readline and assigns it by using a mutable reference to input
+                                                // Reference is essentially a pointer with a guarenteed lifetime and ownership concept
+                                                // See reference test in src\learn\references
+
+
+
+        match input.trim().parse::<i32>() {     // Parses the variable "input(string)" to an i32
+            Ok(number) => {                // If ok it assigns it to a newly created variable "number"
+                return number;                  // Returns number to the calling function
+            }
+            Err(_) => {
+                println!("Invalid input. Please enter a valid number.");
+                continue;                       // Continue the loop if parsing fails
             }
         }
+    };
+
+}
+
+
+fn main() {
+    let mut timer = 0;
+    println!("Enter max desired AFK time (in seconds)");
+
+
+    let usertime = get_user_time();
+    loop {
+        let cursor_locationold = autopilot::mouse::location(); // TODO. Move cursor checking and counter incrementing to their own functions
+        std::thread::sleep(Duration::from_secs(1));
+        let cursor_locationnew = autopilot::mouse::location();
+        if cursor_locationold == cursor_locationnew {
+            timer += 1;
+            println!("{}", timer);
+        } else {
+            timer = 0;
+            println!("{}", timer);
+        }
+
+
+        if timer >= usertime {
+            println!("You are AFK");
+            break;
+        }
+    }
+}
